aeb910b121575c260c90e292146b1bde4f64ea06
erohin-danila <63481577+erohin-danila@users.noreply.github.com>
2021-02-13 18:36:54 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+    });
+});
+$(document).ready(function() {
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('fixed-page');
+    });
+});
