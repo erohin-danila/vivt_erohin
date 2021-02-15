9d08ab25b3aacee6687460349abad18c89ae0fc2
erohin-danila <63481577+erohin-danila@users.noreply.github.com>
2021-02-15 23:27:57 +0300

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+var events = [
+  {'Date': new Date(2018, 09, 15), 'Title': '1'},
+  {'Date': new Date(2018, 10, 18), 'Title': '2'},
+  {'Date': new Date(2016, 12, 27), 'Title': '3'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
